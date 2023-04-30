e68103866af83c7dd3532c7b5820c7d86dec4bde
Alina <almaksimovic01@gmail.com>
2023-04-30 23:12:03 +0300

    init: start project

---
+++ startup-summer-2023/pages/_app.tsx
@@ -0,0 +1,31 @@
+import { AppProps } from "next/app";
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
+
+import "@/styles/globals.scss";
+
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: "light",
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
+}
